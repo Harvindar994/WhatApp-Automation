5c1b5660c160a9b2a88eff6573e588ea3e06e688
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2022-04-03 10:47:36 +0530

    Function to extract QR code
    function completed to extract qr code from webpage

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import *
 import urllib.request
-
+from PIL import Image
 
 class ChromeBrowser(webdriver.Chrome):
     def __init__(self, user_data_dir=f"{os.path.abspath(os.getcwd())}\\Chrome\\userdata"):
@@ -45,6 +45,13 @@ class WhatsApp:
         self.XPATH_MESSAGE_BOX = "//div[@id='main']/footer/div[1]/div[1]/span[2]/div[1]/div[2]/div[1]/div[1]/div[2]"
         self.XPATH_SEND_BUTTON = "//div[@id='main']/footer/div[1]/div[1]/span[2]/div[1]/div[2]/div[2]/button"
         self.XPATH_CHAT_LIST = "//div[@aria-label='Chat list']"
+
+        # XPATH's for Login screen (Loading window)
+        self.XPATH_QR_CODE = "//canvas[@aria-label='Scan me!']"
+        self.XPATH_RELOAD_QR = "//div[@class='landing-window']/div[1]/div[1]/div[2]/div[1]/span[1]/button"
+
+        # Dir for Temporary data and files.
+        self.TEMP_DIR = "temp"
 
         # opening Google Chrome.
         self.Browser = ChromeBrowser()
@@ -131,6 +138,82 @@ class WhatsApp:
     def save_image(self, url):
         pass
 
+    def get_qr_code(self):
+        """
+        Note: at every 20sec cycle qr code will get update.
+        :return: qr code path.
+        """
+        # Here let's check status of qr code, is there any need to reload qr or not.
+        try:
+            # let's locate the reload button on page
+            reload_button = self.Browser.find_element(by=By.XPATH, value=self.XPATH_RELOAD_QR)
+            reload_button.click()
+        except NoSuchElementException:
+            pass
+
+        # Here let's check if qr code is still loading.
+        reload_button_bool = False
+        qr_code_bool = False
+        try:
+            # if it's not able to find qr code that's mean it's not there.
+            self.Browser.find_element(by=By.XPATH, value=self.XPATH_QR_CODE)
+        except NoSuchElementException:
+            qr_code_bool = True
+
+        try:
+            # if it's not able to find reload button that's mean it's not there.
+            self.Browser.find_element(by=By.XPATH, value=self.XPATH_RELOAD_QR)
+        except NoSuchElementException:
+            reload_button_bool = True
+
+        # if both of element( reload button and qr_code) that's mean it's loading the qr code.
+        if reload_button_bool and qr_code_bool:
+            return None
+
+        """
+        Here let's extract the qr code from web page.
+        1. will take screenshot of whole page
+        2. then it will find the position and height and width of qr code
+        3. with the help of PIL library, will crop qr code from screenshot.
+        4. Done.
+        """
+        # creating path of screen_shot file.
+        screenshot = os.path.join(self.TEMP_DIR, "screenshot.png")
+        os.remove(screenshot)
+
+        # First taking Screen Shot of whole page.
+        self.Browser.save_screenshot(screenshot)
+
+        # Here locating Qr code.
+        try:
+            qr_code = self.Browser.find_element(by=By.XPATH, value=self.XPATH_QR_CODE)
+        except NoSuchElementException:
+            print("Log: Unable to locate qr code")
+            return None
+
+        # Here extracting information about size and potion of qr code on page.
+        qr_location = qr_code.location
+        qr_size = qr_code.size
+
+        padding = 5
+        x = qr_location['x'] - padding
+        y = qr_location['y'] - padding
+        width = x + qr_size['width'] + (padding * 2)
+        height = y + qr_size['height'] + (padding * 2)
+
+        # Here let's crop qr code from screenshot.
+        try:
+            qr_code_image = os.path.join(self.TEMP_DIR, 'qr_code.png')
+            os.remove(qr_code_image)
+            im = Image.open(screenshot)
+            im = im.crop((int(x), int(y), int(width), int(height)))
+            im.save(qr_code_image)
+        except:
+            print("Log: Unable to crop qr code from screenshot.")
+            return None
+
+        return qr_code_image
+
     def close(self):
         self.Browser.close()
 
@@ -142,7 +225,8 @@ while True:
     print("1. Get List of active chats")
     print("2. Search Element by XPath")
     print("3. Send Message")
-    print("3. Exit")
+    print("5. Save Qr Code")
+    print("4. Exit")
     choice = int(input("Enter Your Choice: "))
 
     if choice == 1:
@@ -161,5 +245,8 @@ while True:
 
     elif choice == 4:
         break
+
+    elif choice == 5:
+        print(whats_app.get_qr_code())
 
 whats_app.close()
